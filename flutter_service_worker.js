3bb770ae215759a54c2bdc9395727ef6be54074d
Nguyễn Giang Vĩ Sơn <gnvisons@gmail.com>
2022-01-06 17:52:09 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,19 +3,25 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "8dd8c88d92b250ae99c477bfa22d9e36",
-"index.html": "512ceb42b367536e160367b41d6702ee",
-"/": "512ceb42b367536e160367b41d6702ee",
-"main.dart.js": "ccc26b9ddc679dbf553539adbce94351",
+  "version.json": "bbfc689a65b33cee825a2f39e406f564",
+"index.html": "d128ed7ff77722712418a6256605a495",
+"/": "d128ed7ff77722712418a6256605a495",
+"main.dart.js": "e78971c3230e0c01b2b2872717983c76",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "1ea9d665b86858aeb5e02a7110b9fcf8",
+"manifest.json": "7e89e4612eab03a889208ca92fc4e8d8",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "bde9ee889e7cdb2c908965b81d635c52",
+"assets/NOTICES": "172661f7a237bd44c35101faf594c151",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
+"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
+"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
+"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498"
 };
 
 // The application shell files that are downloaded before a service worker can
